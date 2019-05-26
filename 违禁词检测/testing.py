27d4cf2c00f4e2ec0d3e6d4f34b65f27dd7d92f1
yemanzhongting <838044557@qq.com>
2019-05-26 20:13:57 +0800

    改写

---
+++ 违禁词检测/testing.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'zy'
+__time__ = '2019/5/26 13:58'
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+if __name__ == "__main__":
+
+    parse(r"违禁词")
+    print (filter("我爱学习", "*"))
+    print (filter("针孔摄像机", "*"))
+    print (filter("售假人民币", "*"))
+    print (filter("传世私服", "*"))
